286f6daaaf6f3a95ed7e2470f0024679bc6c8ef6
Kimshubb <issakimanifmk@gmail.com>
2024-09-10 07:58:14 +0000

    aync basics

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -8,6 +8,6 @@ async def wait_random(max_delay: int 10) -> float:
     """
     Waits for a random number of secs
     """
-    wait_time = random.random() * max_delay
+    wait_time = random.uniform(0, max_delay)
     await ayncio.sleep(wait_time)
     return wait_time
