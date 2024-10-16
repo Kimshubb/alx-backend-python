5ff09066addc3ce61be00cc3279c6af27a57f4d8
Kimshubb <issakimanifmk@gmail.com>
2024-10-16 18:23:04 +0000

    chesks

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -5,11 +5,9 @@
 def concat(str1: str, str2, str) -> str:
     """
     Concatenate two strings and return the result as a string.
-
     Parameters:
     str1 (str): The first string.
     str2 (str): The second string.
-
     Returns:
     str: The concatenated result of str1 and str2.
     """
